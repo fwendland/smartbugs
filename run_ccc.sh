3479abe7b4bbede7d98734e32f6d2e23d40f63cd
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2023-02-27 11:10:34 +0100

    Update V1 mapping

--- run_ccc.sh
+++ run_ccc.sh
@@ -1,9 +1,10 @@
 python3 smartBugs.py -d access_control -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
-#python3 smartBugs.py -d arithmetic -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
-#python3 smartBugs.py -d bad_randomness -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
-#python3 smartBugs.py -d denial_of_service -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
-#python3 smartBugs.py -d front_running -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
+python3 smartBugs.py -d arithmetic -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
+python3 smartBugs.py -d bad_randomness -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
+python3 smartBugs.py -d denial_of_service -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
+python3 smartBugs.py -d front_running -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
 python3 smartBugs.py -d reentrancy -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
-#python3 smartBugs.py -d short_addresses -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
-#python3 smartBugs.py -d time_manipulation -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
+python3 smartBugs.py -d short_addresses -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
+python3 smartBugs.py -d time_manipulation -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
 python3 smartBugs.py -d unchecked_ll_calls -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
+python3 smartBugs.py -d other -t ccc --skip-existing --execution-name curated --processes 1 --output-version v1
